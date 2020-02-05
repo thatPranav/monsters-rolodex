f2f4b9541802d661a4a72e50786b00111968b803
thatpranav <jawajipranav@gmail.com>
2020-02-05 18:25:49 +0530

    Updates

---
+++ precache-manifest.18a69866601deb4f901fbd4be13e5aca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d694e67e359766761b7b865f4149f00",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0c238342a1ea02fc1c92",
+    "url": "/monsters-rolodex/static/css/main.b535d4d4.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c238342a1ea02fc1c92",
+    "url": "/monsters-rolodex/static/js/main.d1f915a0.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
